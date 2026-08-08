947d88ef1aa1c7c1e2292ccf22e4f1a880b262a1
MiguelLima <mlimacamello@gmail.com>
2026-08-08 19:41:04 -0300

    super otimização do codigo

--- assents.py
+++ assents.py
@@ -9,22 +9,125 @@ RPG = """
         ██║  ██║██║     ╚██████╔╝
         ╚═╝  ╚═╝╚═╝      ╚═════╝
 """
+EDICAO = """
+========================================
+|           EDIÇÃO DA FICHA            |
+========================================
+|                                      |
+|   O que deseja editar?               |
+|                                      |
+|   [1] Nível       [5] Agilidade      |
+|   [2] Mana        [6] Vitalidade     |
+|   [3] Força       [7] Resistência    |
+|   [4] Itens       [8] Inteligência   |
+|                                      |
+|   [0] Continuar                      |
+|                                      |
+========================================
+"""
+MENU = """
+========================================
+|            MENU PRINCIPAL            |
+========================================
+|                                      |
+|  O que deseja fazer?                 |
+|                                      |
+|  [1] Criar ficha de RPG              |
+|  [2] Consultar ficha                 |
+|  [3] Listar todas as fichas          |
+|  [4] Excluir ficha                   |
+|  [5] Editar ficha                    |
+|                                      |
+|  [0] Fecha programa                  |
+|                                      |
+========================================
+"""
+RANDOM = """
+========================================
+|       Deseja editar ficha ou         |
+|       randomizar os status?          |
+========================================
+|                                      |
+|   [1] Editar Status                  |
+|   [2] Randomizar status              |
+|                                      |
+|   [0] Ficha em branco                |
+|                                      |
+========================================
+"""
+SALVAR = """
+========================================
+|           DESEJA SALVAR?             |
+========================================
+|                                      |
+|   [S] Salvar ficha                   |
+|   [N] Não salvar                     |
+|                                      |
+|   [0] Fechar programa                |
+|                                      |
+========================================
+"""
+EXCLUIR = """
+========================================
+|       DESEJA EXCLUIR FICHA?          |
+========================================
+|                                      |
+|   [S] Excluir ficha                  |
+|   [N] Não excluir                    |
+|                                      |
+|   [0] Fechar programa                |
+|                                      |
+========================================
+"""
+ATUALIZAR = """
+========================================
+|       DESEJA ATUALIZAR FICHA?        |
+========================================
+|                                      |
+|   [S] Atualizar ficha                |
+|   [N] Não atualizar                  |
+|                                      |
+|   [0] Fechar programa                |
+|                                      |
+========================================
+"""
 
 def cls():
     os.system("cls")
 
-def trocar_tela():
-    loading(0.01)
+def trocar_tela(load=True):
+    if load:
+        loading(0.01)
     cls()
     print(RPG)
-
 
 def loading(delay=0.1):
     por = 0
     for _ in range(10):
         for _ in range(10):
             barra = por // 10
-            print(f"Loading[{'▉'*barra:<10}]{por}%", end="\r")
+            print(f"  Carregando[{'▉'*barra:<10}]{por}%", end="\r")
             por += 1
             time.sleep(delay)
-    print(f"Loading[{'▉'*10}]100%")
+    print(f"  Carregado[{'▉'*10}]100%")
+
+def pergunta(mensagem=""):
+    while True:
+        print(mensagem)
+        resposta = input("> ").strip().lower()
+
+        if resposta in ("s", "n", "0"):
+            return resposta
+        print("  Opção inválida!")
+
+def opção(num0, num, mensagem=""):
+    while True:
+        print(mensagem)
+        try:
+            op = int(input("> ").strip())
+
+            if op in range(num0, num+1):
+                return str(op)
+            print(f"  Opção inválida! digite um numero entre {num0} e {num}")
+        except:
+            print(f"  Digito inválido! digite um numero")
--- main.py
+++ main.py
@@ -4,38 +4,14 @@ from assents import *
 import time
 import sys
 
-
 # frufru de inicialização
 cls()
-print(RPG)
 loading(0.05)
-time.sleep(1)
-print("\ninicialização completa!")
 time.sleep(1)
 
 while True:
-    cls()
-    print(RPG)
-
-    print("""
-========================================
-|            MENU PRINCIPAL            |
-========================================
-|                                      |
-|  O que deseja fazer?                 |
-|                                      |
-|  [1] Criar ficha de RPG              |
-|  [2] Consultar ficha                 |
-|  [3] Listar todas as fichas          |
-|  [4] Excluir ficha                   |
-|  [5] Editar ficha                    |
-|                                      |
-|  [0] Fecha programa                  |
-|                                      |
-========================================
-""")
-
-    menu_principal = input("> ")
+    trocar_tela(False)
+    menu_principal = opção(0,5, MENU)
     trocar_tela()
 
     # 1 opção criar
@@ -55,49 +31,21 @@ while True:
 ========================================
 """)
         # criando objeto
-        nome = input("  Nome: ").strip().capitalize()
+        nome = input("  Nome: ").strip().title()
         raça = input("  Raça: ").strip()
         genero = input("  Gênero: ").strip()
         classe = input("  Classe: ").strip()
         ficha_ = Jogador(nome, raça, genero, classe)
 
-        print("""
-========================================
-|       Deseja editar ficha ou         | 
-|       randomizar os status?          |
-========================================
-|                                      |
-|   [1] Editar Status                  |
-|   [2] Randomizar status              |
-|                                      |
-========================================
-""")
-        
-        editar = input("> ").strip()
-        # trocar_tela()
+        editar = opção(0,2, RANDOM)
+        trocar_tela()
 
         if editar == "1":
             while True:
-                trocar_tela()
-                ficha_.mostrar_ficha
-                print("""
-========================================
-|           EDIÇÃO DA FICHA            |
-========================================
-|                                      |
-|   O que deseja alterar?              |
-|                                      |
-|   [1] Nível       [5] Agilidade      |
-|   [2] Mana        [6] Vitalidade     |
-|   [3] Força       [7] Resistência    |
-|   [4] Itens       [8] Inteligência   |
-|                                      |
-|   [0] Continuar                      |
-|                                      |
-========================================
-""")
-                
-                edição_ficha = input("> ")
+                trocar_tela(False)
+                ficha_.mostrar_ficha                
+                edição_ficha = opção(0,8, EDICAO)
+
                 # Nivel
                 if edição_ficha == "1":
                     print("  Novo valor do Nível:")
@@ -152,35 +100,15 @@ while True:
                     trocar_tela()
                     break
 
-                else:
-                    print("  Opção inválida, digite um numero de 0 a 6")
-                    time.sleep(1)
-
         elif editar == "2":
             atributos = ["mana", "força", "agilidade", "vitalidade", "resistencia", "inteligencia"]
             for att in atributos:
                 ficha_.randomizer(att)
 
-        else:
-            print("  Opção Inválida, ficha não editada!")
-            time.sleep(1)
-            cls()
-
         while True:
             ficha_.mostrar_ficha
-            print("""
-========================================
-|           DESEJA SALVAR?             |
-========================================
-|                                      |
-|   [S] salvar ficha                   |
-|   [N] não salvar                     |
-|                                      |
-|   [0] não salvar e fecha o programa  |
-|                                      |
-========================================
-""")    
-            salvar = input("> ").strip().lower()
+            salvar = pergunta(SALVAR)
+
             if salvar == "s":
                 save(ficha_)
                 print("  Ficha salva com sucesso!")
@@ -195,8 +123,6 @@ while True:
                 time.sleep(1)
                 cls()
                 sys.exit()           
-            else:
-                print(f"  Opção inválida, digite S, N ou 0.")
 
         # 2 opção buscar
     elif menu_principal == "2":
@@ -210,13 +136,13 @@ while True:
 |                                      |
 ========================================
 """)
-        busca = input("> ").strip().capitalize()
+        busca = input("> ").strip().title()
         listar_fichas(busca)
 
         print("  Insira 0 para voltar")
         voltar = input("> ").strip()
         if voltar != "0":
-            print(" Aperta 0 burro, vou deixar passar dessa vez")
+            print(" Aperta 0 burro, esquece eu saio para vc")
             time.sleep(2)
 
         # 3 opção lista
@@ -232,12 +158,10 @@ while True:
         print("  Insira 0 para voltar")
         voltar = input("> ").strip()
         if voltar != "0":
-            print(" Aperta 0 burro, vou deixar passar dessa vez")
+            print(" Aperta 0 burro, esquece eu saio para vc")
             time.sleep(2)
 
-
     elif menu_principal == "4":
-        cls()
         print("""
 ========================================
 |            EXCLUIR FICHA             |
@@ -256,19 +180,7 @@ while True:
 
             trocar_tela()
             ficha_excluir.mostrar_ficha
-            print("""
-========================================
-|       DESEJA EXCLUIR FICHA?          |
-========================================
-|                                      |
-|   [S] Excluir ficha                  |
-|   [N] não excluir                    |
-|                                      |
-|   [0] não excluir fechar             |
-|                                      |
-========================================
-            """)
-            certeza = input("> ").strip().lower()
+            certeza = pergunta(EXCLUIR)
 
             if certeza == "s":
                 deletar(id_excluir)
@@ -281,167 +193,121 @@ while True:
                 print("  Fechando...")
                 time.sleep(2)
                 cls()
-                sys.exit()
+                break
                 
-
         except AttributeError:
             print("  Esse id não existe!!!")
             time.sleep(2)
 
         # opção 5 edição json
     elif menu_principal == "5":
-        while True:
-            cls()
-            print("""
+        print("""
 ========================================
 |  Insira um id existente para edição  |
 ========================================
-""")
-            try:
-                id_edição = int(input("> ").strip())
-                ficha_edit = carregar_json(id_edição)
+    """)
+        try:
+            id_edição = int(input("> ").strip())
+            ficha_edit = carregar_json(id_edição)
 
-                ficha_edit.mostrar_ficha
-                print("  Essa é a correta? S/N")
-                correta = input("> ").strip().lower()
+            ficha_edit.mostrar_ficha
+            correta = pergunta("Essa é a correta? S/N, 0 para sair")
 
-                if correta == "s":
-                    while True:
+            if correta == "s":
+                while True:
+                    trocar_tela(False)
+                    ficha_edit.mostrar_ficha                                    
+                    edição_ficha = opção(0,8, EDICAO)
+
+                    # Nivel
+                    if edição_ficha == "1":
+                        print("  Novo valor do Nível:")
+                        ficha_edit.atualizar_status("nivel", int(input("> ")))
+                        print("  Valor do Nível alterado com sucesso!")
+                        time.sleep(1)
+                    # Mana
+                    elif edição_ficha == "2":
+                        print("  Novo valor da Mana:")
+                        ficha_edit.atualizar_status("mana", int(input("> ")))
+                        print("  Valor da Mana alterada com sucesso!")
+                        time.sleep(1)
+                    # Força
+                    elif edição_ficha == "3":
+                        print("  Novo valor da Força:")
+                        ficha_edit.atualizar_status("força", int(input("> ")))
+                        print("  Valor da Força alterada com sucesso!")
+                        time.sleep(1)
+                    # Itens
+                    elif edição_ficha == "4":
+                        print("  Informe os itens separados por virgula e espaço (, ):")
+                        novos_items = input("> ").split(", ")
+                        ficha_edit.adicionar_itens(*novos_items)
+                        print(f"  Itens adicionados com sucesso!")
+                        time.sleep(2)
+                    # Agilidade
+                    elif edição_ficha == "5":
+                        print("  Novo valor da Agilidade:")
+                        ficha_edit.atualizar_status("agilidade", int(input("> ")))
+                        print("  Valor da Agilidade alterada com sucesso!")
+                        time.sleep(1)
+                    # Vitalidade
+                    elif edição_ficha == "6":
+                        print("  Novo valor da Vitalidade:")
+                        ficha_edit.atualizar_status("vitalidade", int(input("> ")))
+                        print("  Valor da Vitalidade alterada com sucesso!")
+                        time.sleep(1)
+                    # Resistencia
+                    elif edição_ficha == "7":
+                        print("  Novo valor da Resistência:")
+                        ficha_edit.atualizar_status("resistencia", int(input("> ")))
+                        print("  Valor da Resistência alterada com sucesso!")
+                        time.sleep(1)
+                    # Inteligencia
+                    elif edição_ficha == "8":
+                        print("  Novo valor da Inteligência:")
+                        ficha_edit.atualizar_status("inteligencia", int(input("> ")))
+                        print("  Valor da Inteligência alterada com sucesso!")
+                        time.sleep(1)
+
+                    elif edição_ficha == "0":
                         trocar_tela()
-                        ficha_edit.mostrar_ficha
-                        print("""
-========================================
-|           EDIÇÃO DA FICHA            |
-========================================
-|                                      |
-|   O que deseja alterar?              |
-|                                      |
-|   [1] Nível       [5] Agilidade      |
-|   [2] Mana        [6] Vitalidade     |
-|   [3] Força       [7] Resistência    |
-|   [4] Itens       [8] Inteligência   |
-|                                      |
-|   [0] Continuar                      |
-|                                      |
-========================================
-                        """)
-                                        
-                        edição_ficha = input("> ")
-                        # Nivel
-                        if edição_ficha == "1":
-                            print("  Novo valor do Nível:")
-                            ficha_edit.atualizar_status("nivel", int(input("> ")))
-                            print("  Valor do Nível alterado com sucesso!")
-                            time.sleep(1)
-                        # Mana
-                        elif edição_ficha == "2":
-                            print("  Novo valor da Mana:")
-                            ficha_edit.atualizar_status("mana", int(input("> ")))
-                            print("  Valor da Mana alterada com sucesso!")
-                            time.sleep(1)
-                        # Força
-                        elif edição_ficha == "3":
-                            print("  Novo valor da Força:")
-                            ficha_edit.atualizar_status("força", int(input("> ")))
-                            print("  Valor da Força alterada com sucesso!")
-                            time.sleep(1)
-                        # Itens
-                        elif edição_ficha == "4":
-                            print("  Informe os itens separados por virgula e espaço (, ):")
-                            novos_items = input("> ").split(", ")
-                            ficha_edit.adicionar_itens(*novos_items)
-                            print(f"  Itens adicionados com sucesso!")
-                            time.sleep(2)
-                        # Agilidade
-                        elif edição_ficha == "5":
-                            print("  Novo valor da Agilidade:")
-                            ficha_edit.atualizar_status("agilidade", int(input("> ")))
-                            print("  Valor da Agilidade alterada com sucesso!")
-                            time.sleep(1)
-                        # Vitalidade
-                        elif edição_ficha == "6":
-                            print("  Novo valor da Vitalidade:")
-                            ficha_edit.atualizar_status("vitalidade", int(input("> ")))
-                            print("  Valor da Vitalidade alterada com sucesso!")
-                            time.sleep(1)
-                        # Resistencia
-                        elif edição_ficha == "7":
-                            print("  Novo valor da Resistência:")
-                            ficha_edit.atualizar_status("resistencia", int(input("> ")))
-                            print("  Valor da Resistência alterada com sucesso!")
-                            time.sleep(1)
-                        # Inteligencia
-                        elif edição_ficha == "8":
-                            print("  Novo valor da Inteligência:")
-                            ficha_edit.atualizar_status("inteligencia", int(input("> ")))
-                            print("  Valor da Inteligência alterada com sucesso!")
-                            time.sleep(1)
+                        break
+
+                while True:
+                    ficha_edit.mostrar_ficha
+                    atualiza = pergunta(ATUALIZAR)
+
+                    if atualiza == "s":
+                        atualizar(ficha_edit)
+                        print("  Ficha atualizada com sucesso!")
+                        time.sleep(2)
+                        break
+
+                    elif atualiza == "n":
+                        print("  Ficha não salva.")
+                        time.sleep(2)
+                        break
+
+                    elif atualiza == "0":
+                        print("  Fechando...")
+                        time.sleep(2)
+                        cls()
+                        sys.exit()
+                    
+            elif correta == "n":
+                cls()
+                continue
+
+            elif correta == "0":
+                cls()
+                break
         
-                        elif edição_ficha == "0":
-                            trocar_tela()
-                            break
-        
-                        else:
-                            print("  Opção inválida, digite um numero de 0 a 6")
-                            time.sleep(1)
-
-                    while True:
-                        ficha_edit.mostrar_ficha
-                        print("""
-========================================
-|       DESEJA ATUALIZAR FICHA?        |
-========================================
-|                                      |
-|   [S] atualizar ficha                |
-|   [N] não atualizar                  |
-|                                      |
-|   [0] não atualizar e fecha          |
-|                                      |
-========================================
-                        """)
-                        atualiza = input("> ").strip().lower()
-                        menu = False # gambiarrinha pra voltar pro menu
-
-                        if atualiza == "s":
-                            atualizar(ficha_edit)
-                            print("  Ficha atualizada com sucesso!")
-                            menu = True
-                            time.sleep(2)
-                            break
-
-                        elif atualiza == "n":
-                            print("  Ficha não salva.")
-                            time.sleep(2)
-                            menu = True
-                            break
-
-
-                        elif atualiza == "0":
-                            print("  Fechando...")
-                            time.sleep(2)
-                            sys.exit()
-
-                        else:
-                            print("  Resposta inválida.")
-                            time.sleep(2)
-                    if menu: break
-
-
-
-                elif correta == "n":
-                    cls()
-                    continue
-
-                else:
-                    print("  Resposta inválida")
-                    time.sleep(2)
-            
-            except AttributeError:
-                print("  Esse id não existe!!!")
-                time.sleep(2)
-            except ValueError:
-                print(" Insira um id, não um digito!!!")
-                time.sleep(2)
+        except AttributeError:
+            print("  Esse id não existe!!!")
+            time.sleep(2)
+        except ValueError:
+            print("  Insira um id, não um digito!!!")
+            time.sleep(2)
 
         # 5 opção sair
     elif menu_principal == "0":
